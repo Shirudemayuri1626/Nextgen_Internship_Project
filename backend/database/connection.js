0cc3efbb95b7465f33c437615fe4201fee1407d7
Shirudemayuri1626 <mayurishirude27@gmail.com>
2025-04-01 18:48:52 +0530

    Initial commit

---
+++ backend/database/connection.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const connection = () => {
+
+  console.log(process.env.MONGO_URI);
+  
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Connected to database.");
+    })
+    .catch((err) => {
+      console.log(`Some error occured while connecting to database: ${err}`);
+    });
+};
